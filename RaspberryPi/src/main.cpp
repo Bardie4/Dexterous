7955a8de0e34cba8503becd73f3c69c808657cad
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-05 14:33:21 +0100

    Bugfix

--- RaspberryPi/src/main.cpp
+++ RaspberryPi/src/main.cpp
@@ -19,8 +19,7 @@ int main()
     int length=16;
     while (1){
         spi_buffer[0]=0x0000;
-        wiringPiSPIDataRW (SENSOR_CHANNEL, spi_buffer, length) ;
-        theta=spi_buffer;
+        theta=wiringPiSPIDataRW (SENSOR_CHANNEL, spi_buffer, length):
         cout << "Angle: " << theta << endl;
     }
 }
