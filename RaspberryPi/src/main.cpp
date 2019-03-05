2ed53e3d9aa7e93b85b1cd240a02a88068da0c14
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-05 13:07:31 +0100

    SPI test1. Added makefile

---
+++ RaspberryPi/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <wiringPiSPI.h>
+using namespace std;
+
+
+
+static const int SENSOR_CHANNEL = 0;
+static const int ESP_CHANNEL = 1;
+static const int SPI_SCLK_FREQUENCY=10000000;
+
+int main()
+{           
+    sensor wiringPiSPISetup (SENSOR_CHANNEL, SPI_SCLK_FREQUENCY);
+    //Angle variable
+    double theta; =
+    //Buffer
+    uint16_t spi_buffer;
+    int length=16;
+    while (1){
+        spi_buffer=0x0000;
+        wiringPiSPIDataRW (channel, spi_buffer, length) ;
+        cout << "Angle: " << theta << endl;
+    }
+}
