9f925de1ded84576d985a7ade3d909baac799158
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-06 11:47:01 +0100

    Bugfix

--- RaspberryPi/src/main.cpp
+++ RaspberryPi/src/main.cpp
@@ -2,7 +2,6 @@
 #include <errno.h>
 #include <wiringPiSPI.h>
 #include <unistd.h>
-#include <errno.h> 
 using namespace std;
 
 
@@ -15,16 +14,15 @@ int main()
 {           
     int sensor;
     sensor = wiringPiSPISetupMode (SENSOR_CHANNEL, SPI_SCLK_FREQUENCY, 0);
-    cout << "Init result: " << strerror(errno) << endl;
+    cout << "Init result: " << sensor << endl;
     //Angle variable
     double theta;
     //Buffer
     unsigned char spi_buffer[2];
-    int length=2;
-    //while (1){
-       //spi_buffer[0]=0x00;
-        //spi_buffer[1]=0x00;
-        //theta=wiringPiSPIDataRW (SENSOR_CHANNEL, spi_buffer, length);
-        //cout << "Angle: " << theta << endl;
-    //}
+    int length=1;
+    while (1){
+       spi_buffer[0]=0x00;
+       theta=wiringPiSPIDataRW (SENSOR_CHANNEL, spi_buffer, length);
+       cout << "Angle: " << theta << endl;
+    }
 }
