6c27ea9f1a34d58b7b3f65ccbc86d8fa9d7cf6ff
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-13 13:01:46 +0100

    final spi test

--- RaspberryPi/src/main.cpp
+++ RaspberryPi/src/main.cpp
@@ -81,10 +81,12 @@ int main()
    set_zero_angle_cmd[1]=0xF1;//0b11110001;
    count = bbSPIXfer(link1, set_zero_angle_cmd, (char *)inBuf, 2); // > DAC
    count = bbSPIXfer(link1, read_angle_cmd, (char *)inBuf, 2);
+   cout  << "Register: " << unsigned(inBuf[0]) <<"| value " << unsigned(inBuf[1]) << endl;
    set_zero_angle_cmd[0]=0x80;
    set_zero_angle_cmd[1]=0xC7;//0b11000111;
    count = bbSPIXfer(link1, set_zero_angle_cmd, (char *)inBuf, 2); // > DAC
    count = bbSPIXfer(link1, read_angle_cmd, (char *)inBuf, 2);
+      cout  << "Register: " << unsigned(inBuf[0]) <<"| value " << unsigned(inBuf[1]) << endl;
 
    //count = bbSPIXfer(link2, read_angle_cmd, (char *)inBuf, 1); // > DAC
   // set_zero_angle_cmd[1]=0b10000001;
