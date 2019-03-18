2a6bf18eb0cfc66eb584bb8a461e8b714a011cbb
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-18 14:18:02 +0100

    Trying ZMQ publisher

--- ZMG_server/gripper_server.c
+++ ZMG_server/gripper_server.c
@@ -9,17 +9,27 @@
 int main (void)
 {
     //  Prepare our context and publisher
+    printf("init\n");
     void *context = zmq_ctx_new ();
+    printf("init\n");
     void *publisher = zmq_socket (context, ZMQ_PUB);
+    printf("init\n");
     zmq_bind (publisher, "tcp://*:5563");
+    printf("init\n");
 
     while (1) {
         //  Write two messages, each with an envelope and content
+        printf("while1\n");
         s_sendmore (publisher, "A");
+        printf("while2\n");
         s_send (publisher, "We don't want to see this");
+        printf("while3\n");
         s_sendmore (publisher, "B");
+        printf("while4\n");
         s_send (publisher, "We would like to see this");
+        printf("while5\n");
         sleep (1);
+        printf("while6\n");
     }
     //  We never get here, but clean up anyhow
     zmq_close (publisher);
