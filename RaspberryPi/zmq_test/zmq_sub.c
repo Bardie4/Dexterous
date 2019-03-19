7a6675257cc6d624282dfae1518f2942d126257e
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-19 11:12:17 +0100

    stuff

--- RaspberryPi/zmq_test/zmq_sub.c
+++ RaspberryPi/zmq_test/zmq_sub.c
@@ -7,7 +7,7 @@ int main (void)
     //  Prepare our context and subscriber
     void *context = zmq_ctx_new ();
     void *subscriber = zmq_socket (context, ZMQ_SUB);
-    zmq_connect (subscriber, "tcp://localhost:5563");
+    zmq_connect (subscriber, "tcp://127.0.0.1:5563");
     zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "B", 1);
 
     while (1) {
