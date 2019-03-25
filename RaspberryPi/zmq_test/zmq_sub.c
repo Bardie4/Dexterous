44b363cb9b2073a52ca7c644907d4183a8bebf9c
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-25 10:56:19 +0100

    basic zmq done

--- RaspberryPi/zmq_test/zmq_sub.c
+++ RaspberryPi/zmq_test/zmq_sub.c
@@ -7,7 +7,10 @@ int main (void)
     //  Prepare our context and subscriber
     void *context = zmq_ctx_new ();
     void *subscriber = zmq_socket (context, ZMQ_SUB);
-    zmq_connect (subscriber, "tcp://10.218.130.229:5563");
+    int link1_angle;
+    int link2_angle;
+    //zmq_connect (subscriber, "tcp://10.218.130.229:5563");
+    zmq_connect (subscriber, "tcp://localhost:5563");
     zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "B", 1);
 
     while (1) {
@@ -15,7 +18,12 @@ int main (void)
         char *address = s_recv (subscriber);
         //  Read message contents
         char *contents = s_recv (subscriber);
-        printf ("[%s] %s\n", address, contents);
+        //printf("%s\n", contents);
+        sscanf(contents, "%d %d", &link1_angle, &link2_angle);
+        //printf("| %s %s\n", garbage1,garbage2);
+        //sscanf(contents, "%lf[^ ]%lf[^\n]", &link1_angle, &link2_angle);
+        printf("%d %d\n", link1_angle, link2_angle);
+        //printf("%s\n", contents);
         free (address);
         free (contents);
     }
