a7be1f03e8cc06a2a4657c748d2e780461e0f9d8
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-25 11:49:58 +0100

    testing multithreading

--- RaspberryPi/zmq_test/zmq_sub.c
+++ RaspberryPi/zmq_test/zmq_sub.c
@@ -3,17 +3,31 @@
 #include "zhelpers.h"
 #include "pthread.h"
 
-void read_reference_angle(char* address, char* contents,void* subscriber , int* link1_angle, int* link2_angle){
+typedef struct read_zmq_bundle {
+   char* address;
+   char* contents;
+   void* subscriber;
+   int link1_angle;
+   int link2_angle;
+}read_zmq_bundle;
+
+
+pthread_t tid[2];
+pthread_mutex_t lock;
+
+void read_reference_angle(read_zmq_bundle* zmq_read){
   while (1) {
       //  Read envelope with address
-      address = s_recv (subscriber);
+      zmq_read->address = s_recv (subscriber);
       //  Read message contents
-      contents = s_recv (subscriber);
+      zmq_read->contents = s_recv (subscriber);
       //printf("%s\n", contents);
-      sscanf(contents, "%d %d", &link1_angle, &link2_angle);
+      pthread_mutex_lock(&lock);
+      sscanf(zmq_read->contents, "%d %d", &zmq_read->link1_angle, &zmq_read->link2_angle);
       //printf("| %s %s\n", garbage1,garbage2);
       //sscanf(contents, "%lf[^ ]%lf[^\n]", &link1_angle, &link2_angle);
-      printf("%d %d\n", link1_angle, link2_angle);
+      printf("%d %d\n", zmq_read->link1_angle, zmq_read->link2_angle);
+      pthread_mutex_unlock(&lock);
       //printf("%s\n", contents);
       free (address);
       free (contents);
@@ -22,20 +36,29 @@ void read_reference_angle(char* address, char* contents,void* subscriber , int* 
 
 int main (void)
 {
+    read_zmq_bundle zmq_read;
     //  Prepare our context and subscriber
-    void *context = zmq_ctx_new ();
-    void *subscriber = zmq_socket (context, ZMQ_SUB);
-    int link1_angle;
-    int link2_angle;
+    zmq_read.context = zmq_ctx_new ();
+    zmq_read.subscriber = zmq_socket (context, ZMQ_SUB);
+
+    //void *context = zmq_ctx_new ();
+    //void *subscriber = zmq_socket (context, ZMQ_SUB);
     //zmq_connect (subscriber, "tcp://10.218.130.229:5563");
-    zmq_connect (subscriber, "tcp://169.254.27.157:5563");
-    zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "B", 1);
+    zmq_connect (zmq_read.subscriber, "tcp://169.254.27.157:5563");
+    zmq_setsockopt (zmq_read.subscriber, ZMQ_SUBSCRIBE, "B", 1);
 
+    pthread_create(&(tid[0]), NULL, &read_reference_angle, &zmq_read);
+    //read_reference_angle(address, contents, subscriber, &link1_angle, &link2_angle);
 
-    char *address ;
-    char *contents;
-    read_reference_angle(address, contents, subscriber, &link1_angle, &link2_angle);
-
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+    
+    while(1){
+      printf("I am now reading from memory modified on another thread: %d | %d",&link1_angle, &link2_angle);
+    }
   //  while (1) {
   //      //  Read envelope with address
   //      char *address = s_recv (subscriber);
