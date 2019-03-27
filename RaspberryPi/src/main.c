9ef4d9e5a8360bab3c03c0f3f076e5ad90d71292
rubenwntnu <rubenw@stud.ntnu.no>
2019-03-27 20:15:21 +0100

    Added support for multiple fungers running on separate threads

--- RaspberryPi/src/main.c
+++ RaspberryPi/src/main.c
@@ -11,31 +11,38 @@
 //#include <bitset>
 #include "pthread.h"
 
-#define link1 5
-#define link2 6
-#define esp 13
-#define MISO 19
-#define MOSI 26
-#define SCLK 21
+#define FREQ 1000000
+#define SCLK 11
+#define MOSI 10
+#define MISO 9
+#
 
-pthread_t tid[2];
+typedef struct spi{
+	int clk;
+	int mosi;
+	int miso;
+	int cs_angle_sensor_1;     //GPIO pins for manual chip select
+	int cs_angle_sensor_2;     //GPIO pins for manual chip select
+	int cs_esp32;              //GPIO pins for manual chip select
+}spi;
+
+const struct spi SPI_F1 = {FREQ ,SCLK, MOSI, MISO, 2, 3, 4};
+const struct spi SPI_F2 = {FREQ ,SCLK, MOSI, MISO, 5, 6, 7};
+const struct spi SPI_F3 = {FREQ ,SCLK, MOSI, MISO, 12, 13, 14};
+const struct spi SPI_F4 = {FREQ ,SCLK, MOSI, MISO, 15, 16, 17};
+const struct spi SPI_F5 = {FREQ ,SCLK, MOSI, MISO, 18, 19, 20};
+//There is enough GPIO pins for a total of 7 fingers.
+//Anything above two fingers will not be tested.
+//Only cartesian and joint space controllers have support for more than 2 fingers.
+//This is because the fingers are not controlled induvidually in the other controllers
+
+pthread_t tid[6];
+
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-//Variables used by ZMQ, shared by all two threads, useable by all controllers
-typedef struct zmq_bundle {
-   char* address;
-   char* contents;
-   void* context;
-   void* subscriber;
-   char function_flag;
-   int link1_angle;
-   int link2_angle;
-}zmq_bundle;
-
-//Variables used by PID function
+//Variables used by joint space PID function
 typedef struct jointspace_pid_var {
-   zmq_bundle* zmq_bundle;
-   char read_angle_cmd[2];        //Set as 16bit in case of upgrade
+   char read_angle_cmd[2];
    unsigned char inBuf[2];
    short theta1;
    short theta2;
@@ -50,10 +57,9 @@ typedef struct jointspace_pid_var {
    char run;
 }jointspace_pid_var;
 
-//Variables used by PID function
+//Variables used by cartesian PID function
 typedef struct cartesian_pid_var {
-   zmq_bundle* zmq_bundle;
-   char read_angle_cmd[2];        //Set as 16bit in case of upgrade
+   char read_angle_cmd[2];
    unsigned char inBuf[2];
    short theta1;
    short theta2;
@@ -68,39 +74,120 @@ typedef struct cartesian_pid_var {
    char run;
 }cartesian_pid_var;
 
+typedef struct controller_variables{
+	cartesian_pid_var	 cs;
+	jointspace_pid_var js;
+}controller_variables;
+
+typedef struct zmq_payload{
+	short data1;
+	short data2;
+	short data3;
+	short data4;
+	short data5;
+}zmq_payload;
+
+typedef struct zmq_instructions{
+	void (*controller)(void*, void*);
+	zmq_payload payload;
+}zmq_instructions;
+
+typedef struct finger_data{
+	zmq_instructions*  zmq;		             //ptr to shared memory
+	zmq_instructions   zmq_local;	   		   //work memory
+	controller_variables controller_var;	 //variables used by controllers
+  spi spi_data;                          //Constants used for SPI
+}finger_data;
+
+typedef struct zmq_data{
+	char* address;
+	char* contents;
+	void* context;
+	void* subscriber;
+	char function_flag;
+	zmq_instructions instr_finger1;
+	zmq_instructions instr_finger2;
+	zmq_instructions instr_finger3;
+	zmq_instructions instr_finger4;
+	zmq_instructions instr_finger5;
+	int finger_select;
+	int controller_select;
+  void* controller_ptr[4];
+}zmq_data;
 
 void* read_zmq_server(void* zmq_read_input){
-  zmq_bundle* zmq_read = (zmq_bundle*) zmq_read_input;
-  while (1) {
-      //printf("while read mutex");
-      //  Read envelope with address
-      pthread_mutex_lock(&lock);
-      zmq_read->address = s_recv (zmq_read->subscriber);
-      //  Read message contents
-      zmq_read->contents = s_recv (zmq_read->subscriber);
-      //printf("%s\n", contents);
-      sscanf(zmq_read->contents, "%c %d %d",&(zmq_read->function_flag) , &(zmq_read->link1_angle), &(zmq_read->link2_angle));
-      //printf("| %s %s\n", garbage1,garbage2);
-      //sscanf(contents, "%lf[^ ]%lf[^\n]", &link1_angle, &link2_angle);
-      //printf("%d %d\n", zmq_read->link1_angle, zmq_read->link2_angle);
-      //printf("%s\n", contents);
-      free (zmq_read->address);
-      free (zmq_read->contents);
 
-    //  printf("while read mutex end");
-      pthread_mutex_unlock(&lock);
-      usleep(1000);
+  //Casting input
+	zmq_data* zmq_read = (zmq_data*) zmq_read_input;
+	while (1) {
+
+    zmq_read->address = s_recv (zmq_read->subscriber);  //  Read envelope with address
+    zmq_read->contents = s_recv (zmq_read->subscriber); //  Read message contents
+
+
+    pthread_mutex_lock(&lock);
+  	//Analyse input
+  	zmq_read->finger_select = zmq_read->contents[0] >> 4;				//4 MSB in first byte represent finger
+  	zmq_read->controller_select = zmq_read->contents[0] & 00001111;			//4 LSB in first byte represent controller
+
+  	//Store payload
+         	if	( zmq_read->finger_select = 0b00000000){
+  		sscanf(zmq_read->contents, "%*c %d %d %d %d %d"	, &(zmq_read->instr_finger1.payload.data1)
+  						 		, &(zmq_read->instr_finger1.payload.data2)
+  								, &(zmq_read->instr_finger1.payload.data3)
+  								, &(zmq_read->instr_finger1.payload.data4)
+  								, &(zmq_read->instr_finger1.payload.data5));
+  		zmq_read->instr_finger1.controller = zmq_read->controller_ptr[zmq_read->controller_select];
+  	}
+  	else if	( zmq_read->finger_select = 0b00000001){
+  		sscanf(zmq_read->contents, "%*c %d %d %d %d %d"	, &(zmq_read->instr_finger2.payload.data1)
+  						 		, &(zmq_read->instr_finger2.payload.data2)
+  								, &(zmq_read->instr_finger2.payload.data3)
+  								, &(zmq_read->instr_finger2.payload.data4)
+  								, &(zmq_read->instr_finger2.payload.data5));
+  		zmq_read->instr_finger1.controller = zmq_read->controller_ptr[zmq_read->controller_select];
+  	}
+  	else if	( zmq_read->finger_select == 0b00000010){
+  		sscanf(zmq_read->contents, "%*c %d %d %d %d %d"	, &(zmq_read->instr_finger3.payload.data1)
+  						 		, &(zmq_read->instr_finger3.payload.data2)
+  								, &(zmq_read->instr_finger3.payload.data3)
+  								, &(zmq_read->instr_finger3.payload.data4)
+  								, &(zmq_read->instr_finger3.payload.data5));
+  		zmq_read->instr_finger1.controller = zmq_read->controller_ptr[zmq_read->controller_select];
+  	}
+  	else if	( zmq_read->finger_select == 0b00000011){
+  		sscanf(zmq_read->contents, "%*c %d %d %d %d %d"	, &(zmq_read->instr_finger4.payload.data1)
+  						 		, &(zmq_read->instr_finger4.payload.data2)
+  								, &(zmq_read->instr_finger4.payload.data3)
+  								, &(zmq_read->instr_finger4.payload.data4)
+  								, &(zmq_read->instr_finger4.payload.data5));
+  		zmq_read->instr_finger1.controller = zmq_read->controller_ptr[zmq_read->controller_select];
+  	}
+  	else if	( zmq_read->finger_select == 0b00000100){
+  		sscanf(zmq_read->contents, "%*c %d %d %d %d %d"	, &(zmq_read->instr_finger5.payload.data1)
+  						 		, &(zmq_read->instr_finger5.payload.data2)
+  								, &(zmq_read->instr_finger5.payload.data3)
+  								, &(zmq_read->instr_finger5.payload.data4)
+  								, &(zmq_read->instr_finger5.payload.data5));
+  		zmq_read->instr_finger1.controller = zmq_read->controller_ptr[zmq_read->controller_select];
+  	}
+
+    free (zmq_read->address);
+    free (zmq_read->contents);
+    pthread_mutex_unlock(&lock);
+  	usleep(1000);
     }
 }
 
-void jointspace_pid(void* input){
+
+void jointspace_pid(void* payload_in, void* vars){
   //Casting input
-  jointspace_pid_var* pid_var = (jointspace_pid_var*) input;
+  zmq_payload* payload = (zmq_payload*) payload_in;
+  controller_variables* controller_vars = (controller_variables*) vars;
   //Run controller
-  while(1){
   //Read SPI SENSORS
-  pid_var->theta1 = 50;        //For thesting without sensor (Remove this) CAST TO SHORT ON REAL IMPLEMENTATION
-  pid_var->theta2 = 50;        //For thesting without sensor (Remove this)
+  controller_vars->js.theta1 = 50;        //For thesting without sensor (Remove this) CAST TO SHORT ON REAL IMPLEMENTATION
+  controller_vars->js.theta2 = 50;        //For thesting without sensor (Remove this)
   /*
   bbSPIXfer(link1, read_angle_cmd, (char *)inBuf, 1); // > DAC
   theta1=inBuf[0];
@@ -110,35 +197,39 @@ void jointspace_pid(void* input){
   */
 
   //Read input, check if controller is still selected (Shared resources)
+  /*
   pthread_mutex_lock(&lock);
   pid_var->theta1_setpoint = pid_var->zmq_bundle->link1_angle;
   pid_var->theta2_setpoint = pid_var->zmq_bundle->link2_angle;
   pid_var->run = (pid_var->zmq_bundle->function_flag == 0b00000001);
+  controller_vars->js
   pthread_mutex_unlock(&lock);
+
   if (!pid_var->run){ //Input data not relevant for this controller
     break;            //Try next controller
-  }
+  }*/
   printf("Jointspace\n");
-
+  controller_vars->js.theta1_setpoint = payload->data1;
+  controller_vars->js.theta2_setpoint = payload->data2;
   //Proportional controller
-  pid_var->error1 = (pid_var->theta1_setpoint - pid_var->theta1);
-  pid_var->u1 = (short) (pid_var->error1*pid_var->kp1);
-  pid_var->error2 = (pid_var->theta2_setpoint - pid_var->theta2);
-  pid_var->u2 = (short) (pid_var->error2*pid_var->kp2);
-  printf("theta1: %d | theta1_setpoint: %d | error1: %d | u1: %d \n", pid_var->theta1 ,pid_var->theta1_setpoint, pid_var->error1, pid_var->u1);
-  printf("theta2: %d | theta2_setpoint: %d | error2: %d | u2: %d \n", pid_var->theta2 ,pid_var->theta2_setpoint, pid_var->error2, pid_var->u2);
+  controller_vars->js.error1 = (controller_vars->js.theta1_setpoint - controller_vars->js.theta1);
+  controller_vars->js.u1 = (short) (controller_vars->js.error1*controller_vars->js.kp1);
+  controller_vars->js.error2 = (controller_vars->js.theta2_setpoint - controller_vars->js.theta2);
+  controller_vars->js.u2 = (short) (controller_vars->js.error2*controller_vars->js.kp2);
+  printf("theta1: %d | theta1_setpoint: %d | error1: %d | u1: %d \n", controller_vars->js.theta1 , controller_vars->js.theta1_setpoint, controller_vars->js.error1, controller_vars->js.u1);
+  printf("theta2: %d | theta2_setpoint: %d | error2: %d | u2: %d \n", controller_vars->js.theta2 , controller_vars->js.theta2_setpoint, controller_vars->js.error2, controller_vars->js.u2);
 
   //Write to ESP32 through SPI
   usleep(1000);
-  }
 }
 
-
-void cartesian_pid_controller(void* input){
+void cartesian_pid_controller(void* payload_in, void* vars){
   //Casting input
-  cartesian_pid_var* pid_var = (cartesian_pid_var*) input;
+  zmq_payload* payload = (zmq_payload*) payload_in;
+  controller_variables* controller_vars = (controller_variables*) vars;
+
   //Run controller
-  while(1){
+    /*
 
     //Read input, check if controller is still selected (Shared resources)
     pthread_mutex_lock(&lock);
@@ -147,46 +238,145 @@ void cartesian_pid_controller(void* input){
     pthread_mutex_unlock(&lock);
     if (!pid_var->run){ //Input not relevant for this controller
       break;            //Try next controller
-    }
+    }*/
     printf("Cartesian\n");
 
     /*
     Insert controller here
     */
 
-    usleep(10000);
-  }
-}
+    usleep(1000);
 
+}
+void* no_controller(void* a,void* b){
+   //Empty controller. used when nothing happens
+}
+/*
 void controller_select(void* jointspace_pid_var, void* cartesian_pid_var){
   while(1){
     jointspace_pid(jointspace_pid_var);
     cartesian_pid_controller(cartesian_pid_var);
   }
 }
+*/
+
+void* run_controller_once(finger_data* finger){
+
+	//READ ZMQ INSTRUCTION FROM SHARED MEMORY.
+  pthread_mutex_lock(&lock);
+	finger->zmq_local = *(finger->zmq);
+  pthread_mutex_unlock(&lock);
+
+	//Execute instructions
+	(*finger->zmq_local.controller)(&finger->zmq_local.payload, &finger->controller_var); //Runninng a pointer to a controller function
+}
+
+finger_controllers(zmq_data* shared){
+
+	//Initialize 5 finger containers
+	finger_data f1_data;
+	finger_data f2_data;
+	finger_data f3_data;
+	finger_data f4_data;
+	finger_data f5_data;
+
+	//Set SPI options
+	f1_data.spi_data = SPI_F1;
+	f2_data.spi_data = SPI_F2;
+	f3_data.spi_data = SPI_F3;
+	f4_data.spi_data = SPI_F4;
+	f5_data.spi_data = SPI_F5;
+
+	//Add pointers to the relevant input data from zmq
+	f1_data.zmq = &(shared->instr_finger1);
+	f2_data.zmq = &(shared->instr_finger2);
+	f3_data.zmq = &(shared->instr_finger3);
+	f4_data.zmq = &(shared->instr_finger4);
+	f5_data.zmq = &(shared->instr_finger5);
+
+  //Set default settings for controllers
+  controller_variables default_cntrl_set;             //Creating controller set
+  default_cntrl_set.js.kp1 = 1;                        //joint space controller vaules
+  default_cntrl_set.js.kp2 = 0.5;                      //joint space controller vaules
+  default_cntrl_set.js.read_angle_cmd[0] = 0b00000000; //SPI command
+  default_cntrl_set.js.read_angle_cmd[1] = 0b00000000; //SPI command
+  default_cntrl_set.cs.kp1 = 1;                        //cartesian space controller values
+  default_cntrl_set.cs.kp2 = 0.5;                      //cartesian space controller values
+  default_cntrl_set.cs.read_angle_cmd[0] = 0b00000000; //SPI command
+  default_cntrl_set.cs.read_angle_cmd[1] = 0b00000000; //SPI command
+
+  f1_data.controller_var = default_cntrl_set;
+  f2_data.controller_var = default_cntrl_set;
+  f3_data.controller_var = default_cntrl_set;
+  f4_data.controller_var = default_cntrl_set;
+  f5_data.controller_var = default_cntrl_set;
+
+  //Select an empty controller by default_widget
+  f1_data.zmq_local.controller = no_controller;
+  f2_data.zmq_local.controller = no_controller;
+  f3_data.zmq_local.controller = no_controller;
+  f4_data.zmq_local.controller = no_controller;
+  f5_data.zmq_local.controller = no_controller;
+
+	//Separate into 5 threads that runs the controller once. The controllers are synched up by joining the threads.
+	while(1){
+
+  	pthread_create(&(tid[1]), NULL, run_controller_once, &f1_data);
+		pthread_create(&(tid[2]), NULL, run_controller_once, &f2_data);
+		pthread_create(&(tid[3]), NULL, run_controller_once, &f3_data);
+		pthread_create(&(tid[4]), NULL, run_controller_once, &f4_data);
+		pthread_create(&(tid[5]), NULL, run_controller_once, &f5_data);
+
+		pthread_join(tid[1], NULL);
+  	pthread_join(tid[2], NULL);
+  	pthread_join(tid[3], NULL);
+  	pthread_join(tid[4], NULL);
+ 		pthread_join(tid[5], NULL);
+
+	}
+}
 
 int main()
 {
   //VARIABLES USED IN FUNCTIONS
-  zmq_bundle zmq_var;
+  zmq_data zmq_var;
   //  Prepare our context and subscriber
   zmq_var.context = zmq_ctx_new ();
   zmq_var.subscriber = zmq_socket (zmq_var.context, ZMQ_SUB);
+  //Assigning controllers to choose from
+  zmq_var.controller_ptr[0] = &jointspace_pid;
+  zmq_var.controller_ptr[1] = &cartesian_pid_controller;
+  zmq_var.controller_ptr[2] = no_controller;
+  zmq_var.controller_ptr[3] = no_controller;
+  //Default controller at startup for each finger
+  zmq_var.instr_finger1.controller = no_controller;
+  zmq_var.instr_finger2.controller = no_controller;
+  zmq_var.instr_finger3.controller = no_controller;
+  zmq_var.instr_finger4.controller = no_controller;
+  zmq_var.instr_finger5.controller = no_controller;
 
+
+
+
+  printf("%p\n",(void*) zmq_var.controller_ptr[0]);
+  printf("%p\n",(void*) zmq_var.controller_ptr[1]);
+  printf("%p\n",(void*) zmq_var.controller_ptr[2]);
+  printf("%p\n",(void*) zmq_var.controller_ptr[3]);
+ /*
   jointspace_pid_var j_pid_var;
-  j_pid_var.zmq_bundle = &zmq_var;
-  j_pid_var.read_angle_cmd[0] = 0b00000000;
-  j_pid_var.read_angle_cmd[1] = 0b00000000;
+  //j_pid_var.zmq_bundle = &zmq_var;
+  j_pid_var.read_angle_cmd[0] = 0b00000000; //SPI command
+  j_pid_var.read_angle_cmd[1] = 0b00000000; //SPI command
   j_pid_var.kp1 = 1;
   j_pid_var.kp2 = 0.5;
 
   cartesian_pid_var c_pid_var;
-  c_pid_var.zmq_bundle = &zmq_var;
-  c_pid_var.read_angle_cmd[0] = 0b00000000;
-  c_pid_var.read_angle_cmd[1] = 0b00000000;
+  //c_pid_var.zmq_bundle = &zmq_var;
+  c_pid_var.read_angle_cmd[0] = 0b00000000; //SPI command
+  c_pid_var.read_angle_cmd[1] = 0b00000000; //SPI command
   c_pid_var.kp1 = 1;
   c_pid_var.kp2 = 0.5;
-
+*/
   //void *context = zmq_ctx_new ();
   //void *subscriber = zmq_socket (context, ZMQ_SUB);
   //zmq_connect (subscrpid_var->iber, "tcp://10.218.130.229:5563");
@@ -198,9 +388,15 @@ int main()
   pthread_create(&(tid[0]), NULL, &read_zmq_server, &zmq_var);
   usleep(1000);
 
-  controller_select(&c_pid_var, &j_pid_var);
+  finger_controllers(&zmq_var);
+  //controller_select(&mc_pid_var, &j_pid_var);
   //pthread_create(&(tid[1]), NULL, &pid, &pid_var);
   pthread_join(tid[0], NULL);
+  pthread_join(tid[1], NULL);
+  pthread_join(tid[2], NULL);
+  pthread_join(tid[3], NULL);
+  pthread_join(tid[4], NULL);
+  pthread_join(tid[5], NULL);
   //pthread_join(tid[1], NULL);
 
   //pthread_create(&(tid[0]), NULL, &read_reference_angle, &zmq_read);
