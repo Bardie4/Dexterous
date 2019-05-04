e34d93e7cf7bb8bf2a3a98b076689e8dc40b0597
rubenwntnu <rubenw@stud.ntnu.no>
2019-05-04 17:51:37 +0200

    moving everything to separate files

--- RaspberryPi/src/js_pos_controller.cpp
+++ RaspberryPi/src/js_pos_controller.cpp
@@ -1,59 +1,59 @@
 #include "controllers/js_pos_controller.h"
 //Customize names that fit your implementation
 JointSpacePosController::JointSpacePosController():controllerEngine(){
-  controllerEngine.iterate = &iterate();
+  controllerEngine.iterate = &iterate;
   //ZmqSub inputs
-  *name1 = cntrllrEngine.data1;
-  *name2 = &cntrllrEngine.data2;
-  *name3 = &cntrllrEngine.data3;
-  *name4 = &cntrllrEngine.data4;
-  *name5 = &cntrllrEngine.data5;
-  *name6 = &cntrllrEngine.data6;
-  *name7 = &cntrllrEngine.data7;
-  *name8 = &cntrllrEngine.data8;
-  *name9 = &cntrllrEngine.data9;
-  *name10 = &cntrllrEngine.data10;
-  *trajSize = &cntrllrEngine.trajSize;
-  *trajTimeStamp = &cntrllrEngine.trajTimeStamp;
-  *trajPosition = &cntrllrEngine.trajPosition;
-  *trajVelocity = &cntrllrEngine.trajVelocity;
-  *trajAcceleration = &cntrllrEngine.trajAcceleration;
+  *name1 = &controllerEngine.data1;
+  *name2 = &controllerEngine.data2;
+  *name3 = &controllerEngine.data3;
+  *name4 = &controllerEngine.data4;
+  *name5 = &controllerEngine.data5;
+  *name6 = &controllerEngine.data6;
+  *name7 = &controllerEngine.data7;
+  *name8 = &controllerEngine.data8;
+  *name9 = &controllerEngine.data9;
+  *name10 = &controllerEngine.data10;
+  *trajSize = &controllerEngine.trajSize;
+  *trajTimeStamp = &controllerEngine.trajTimeStamp;
+  *trajPosition = &controllerEngine.trajPosition;
+  *trajVelocity = &controllerEngine.trajVelocity;
+  *trajAcceleration = &controllerEngine.trajAcceleration;
   //Peripheral inputs
-  *jointAngle1 = &cntrllrEngine.jointAngle;
-  *jointAngle2 = &cntrllrEngine.jointAngle;
-  *angularVel1 = &cntrllrEngine.angularVel1;
-  *angularVel2 = &cntrllrEngine.angularVel2;
-  *angularAcc1 = &cntrllrEngine.angularAcc1;
-  *angularAcc2 = &cntrllrEngine.angularAcc2;
+  *jointAngle1 = &controllerEngine.jointAngle;
+  *jointAngle2 = &controllerEngine.jointAngle;
+  *angularVel1 = &controllerEngine.angularVel1;
+  *angularVel2 = &controllerEngine.angularVel2;
+  *angularAcc1 = &controllerEngine.angularAcc1;
+  *angularAcc2 = &controllerEngine.angularAcc2;
   //Controller output
-  *commandedTorque1 = &cntrllrEngine.commandedTorque1;
-  *commandedTorque2 = &cntrllrEngine.commandedTorque2;
+  *commandedTorque1 = &controllerEngine.commandedTorque1;
+  *commandedTorque2 = &controllerEngine.commandedTorque2;
   //Run time adjustable variables (example: Kp, Ki and so on..)
-  *var1 = &cntrllrEngine.var1;
-  *var2 = &cntrllrEngine.var2;
-  *var3 = &cntrllrEngine.var3;
-  *var4 = &cntrllrEngine.var4;
-  *var5 = &cntrllrEngine.var5;
-  *var6 = &cntrllrEngine.var6;
-  *var7 = &cntrllrEngine.var7;
-  *var8 = &cntrllrEngine.var8;
-  *var9 = &cntrllrEngine.var9;
-  *var10 = &cntrllrEngine.var10;
-  *var11 = &cntrllrEngine.var11;
-  *var12 = &cntrllrEngine.var12;
-  *var13 = &cntrllrEngine.var13;
-  *var14 = &cntrllrEngine.var14;
-  *var15 = &cntrllrEngine.var15;
-  *var16 = &cntrllrEngine.var16;
-  *var17 = &cntrllrEngine.var17;
-  *var18 = &cntrllrEngine.var18;
-  *var19 = &cntrllrEngine.var19;
-  *var20 = &cntrllrEngine.var20;
+  *var1 = &controllerEngine.var1;
+  *var2 = &controllerEngine.var2;
+  *var3 = &controllerEngine.var3;
+  *var4 = &controllerEngine.var4;
+  *var5 = &controllerEngine.var5;
+  *var6 = &controllerEngine.var6;
+  *var7 = &controllerEngine.var7;
+  *var8 = &controllerEngine.var8;
+  *var9 = &controllerEngine.var9;
+  *var10 = &controllerEngine.var10;
+  *var11 = &controllerEngine.var11;
+  *var12 = &controllerEngine.var12;
+  *var13 = &controllerEngine.var13;
+  *var14 = &controllerEngine.var14;
+  *var15 = &controllerEngine.var15;
+  *var16 = &controllerEngine.var16;
+  *var17 = &controllerEngine.var17;
+  *var18 = &controllerEngine.var18;
+  *var19 = &controllerEngine.var19;
+  *var20 = &controllerEngine.var20;
 }
 
 void JointSpacePosController::iterate(){
-  *commandedTorque1 = *name1
-  *commandedTorque1 = *name2
+  *commandedTorque1 = *name1;
+  *commandedTorque1 = *name2;
 }
 
 ControllerEngine* JointSpacePosController::getHandle(){
