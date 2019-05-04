dd1ed520d53a100446830626ee99eabfa355f36b
rubenwntnu <rubenw@stud.ntnu.no>
2019-05-04 16:28:26 +0200

    moving everything to separate files

--- RaspberryPi/src/finger.cpp
+++ RaspberryPi/src/finger.cpp
@@ -1,6 +1,6 @@
 #include <pthread.h>
 #include "controller_engine.h"
-static pthread_mutex_t zmqSubLock = PTHREAD_MUTEX_INITIALIZER;
-static pthread_mutex_t periphLock = PTHREAD_MUTEX_INITIALIZER;
-static pthread_mutex_t begin_control_iteration = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t zmqSubLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t periphLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t begin_control_iteration = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t start_cond = PTHREAD_COND_INITIALIZER;
