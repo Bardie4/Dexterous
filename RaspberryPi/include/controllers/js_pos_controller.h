6de2d8c2caee7edaa62c7458d417d7683daa9c57
rubenwntnu <rubenw@stud.ntnu.no>
2019-05-04 19:30:32 +0200

    moving everything to separate files

--- RaspberryPi/include/controllers/js_pos_controller.h
+++ RaspberryPi/include/controllers/js_pos_controller.h
@@ -51,7 +51,7 @@ class JointSpacePosController {
     float *var19;
     float *var20;
 
-    static void iterateStatic(void *controller_object);
+    static void iterateStatic(void*);
     JointSpacePosController();
     ControllerEngine* getHandle();
     void iterate();
