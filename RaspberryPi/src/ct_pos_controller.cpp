9714b254f949ca3ec3c072bb62e7980716e177ce
rubenwntnu <rubenw@stud.ntnu.no>
2019-05-15 16:20:24 +0200

    finishing moving controllers

--- RaspberryPi/src/ct_pos_controller.cpp
+++ RaspberryPi/src/ct_pos_controller.cpp
@@ -64,11 +64,11 @@ void CartesianPosController::iterate(){
   //Joint space controller
   error1 = jointAngle1Setpoint - (*jointAngle1);
   integral1 += error1 * (step/1000000.0) * (*ki1);
-  (*commandedTorque1) = error1 * (*kp1) + integral1 + angularVel1 * (*kd1);
+  (*commandedTorque1) = error1 * (*kp1) + integral1 + (*angularVel1) * (*kd1);
 
   error2 = jointAngle1Setpoint - (*jointAngle2);
   integral2 += error2 * (step/1000000.0) * (*ki2);
-  (*commandedTorque2) = error2 * (*kp2) + integral2 + angularVel2 * (*kd2);
+  (*commandedTorque2) = error2 * (*kp2) + integral2 + (*angularVel2) * (*kd2);
 
   //Print status every 1000 cycles
 
